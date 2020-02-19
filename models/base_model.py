058e2ea69281b48d67a3d10cbe10683d27d0e998
diego0096 <dfqz93@hotmail.com>
2020-02-19 16:05:07 +0000

    models commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,49 +1,62 @@
 #!/usr/bin/python3
-"""Class BaseModel"""
-
-
-from datetime import datetime
-import json
+'''
+    This module defines the BaseModel class
+'''
 import uuid
+from datetime import datetime
 import models
 
 
 class BaseModel:
-    """Base Class with public instance attributes and methods"""
-
+    '''
+        Base class for other classes to be used for the duration.
+    '''
     def __init__(self, *args, **kwargs):
-        """Instantiates the attributes of the BaseModel class"""
-        if args is not None and len(args) > 0:
-            pass
-        if kwargs:
-            for key, item in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    item = datetime.strptime(item, "%Y-%m-%dT%H:%M:%S.%f")
-                if key not in ['__class__']:
-                    setattr(self, key, item)
-        else:
+        '''
+            Initialize public instance attributes.
+        '''
+        if (len(kwargs) == 0):
             self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
+        else:
+            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
+                                                     "%Y-%m-%dT%H:%M:%S.%f")
+            for key, val in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, val)
 
     def __str__(self):
-        """ Return the human readable print format"""
-        a, b, c = self.__class__.__name__, self.id, self.__dict__
-        return("[{}] ({}) {}".format(a, b, c))
+        '''
+            Return string representation of BaseModel class
+        '''
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
+
+    def __repr__(self):
+        '''
+            Return string representation of BaseModel class
+        '''
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
 
     def save(self):
-        """Shows the newly updated time from time of instance creation"""
+        '''
+            Update the updated_at attribute with new.
+        '''
         self.updated_at = datetime.now()
-        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionay containing all keys/values"""
-        my_dict = {}
-        for key, item in self.__dict__.items():
-            if key in ['created_at', 'updated_at']:
-                my_dict[key] = item.isoformat()
-            else:
-                my_dict[key] = item
-        my_dict['__class__'] = self.__class__.__name__
-        return my_dict
+        '''
+            Return dictionary representation of BaseModel class.
+        '''
+        cp_dct = dict(self.__dict__)
+        cp_dct['__class__'] = self.__class__.__name__
+        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+        return (cp_dct)
