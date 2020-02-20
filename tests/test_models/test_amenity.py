6d6531d6edcb7c9c1bc937d6b40d0729c0b3f6ac
diego0096 <dfqz93@hotmail.com>
2020-02-20 04:26:36 +0000

    tests commit

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -19,16 +19,14 @@ class TestAmenity(unittest.TestCase):
         self.assertIsInstance(new_amenity, BaseModel)
 
     def test_Amenity_attributes(self):
-        '''
-            Test that Amenity class had name attribute.
-        '''
+        '''Test that Amenity class had name attribute.'''
+
         new_amenity = Amenity()
         self.assertTrue("name" in new_amenity.__dir__())
 
     def test_Amenity_attribute_type(self):
-        '''
-            Test that Amenity class had name attribute's type.
-        '''
+        '''Test that Amenity class had name attribute's type.'''
+        
         new_amenity = Amenity()
         name_value = getattr(new_amenity, "name")
         self.assertIsInstance(name_value, str)
