43b484d825f3652c1ba6d4525b24d802efc4b911
diego0096 <dfqz93@hotmail.com>
2020-02-20 04:42:49 +0000

    tests commit

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -10,30 +10,24 @@ from models.place import Place
 
 
 class TestUser(unittest.TestCase):
-    '''
-        Testing Place class
-    '''
+    '''Testing Place class'''
 
     def setUp(self):
-        '''
-            Creates an instance for place.
-        '''
+        '''Creates an instance for place.'''
+
         self.new_place = Place()
 
     def TearDown(self):
         pass
 
     def test_Place_inheritance(self):
-        '''
-            tests that the City class Inherits from BaseModel
-        '''
+        '''tests that the City class Inherits from BaseModel'''
 
         self.assertIsInstance(self.new_place, BaseModel)
 
     def test_Place_attributes(self):
-        '''
-            Checks that the attribute exist.
-        '''
+        '''Checks that the attribute exist.'''
+
         self.assertTrue("city_id" in self.new_place.__dir__())
         self.assertTrue("user_id" in self.new_place.__dir__())
         self.assertTrue("description" in self.new_place.__dir__())
@@ -46,64 +40,55 @@ class TestUser(unittest.TestCase):
         self.assertTrue("amenity_ids" in self.new_place.__dir__())
 
     def test_type_longitude(self):
-        '''
-            Test the type of longitude.
-        '''
+        '''Test the type of longitude.'''
+
         longitude = getattr(self.new_place, "longitude")
         self.assertIsInstance(longitude, float)
 
     def test_type_latitude(self):
-        '''
-            Test the type of latitude
-        '''
+        '''Test the type of latitude'''
+
         latitude = getattr(self.new_place, "latitude")
         self.assertIsInstance(latitude, float)
 
     def test_type_amenity(self):
-        '''
-            Test the type of latitude
-        '''
+        '''Test the type of latitude'''
+
         amenity = getattr(self.new_place, "amenity_ids")
         self.assertIsInstance(amenity, list)
 
     def test_type_price_by_night(self):
-        '''
-            Test the type of price_by_night
-        '''
+        '''Test the type of price_by_night'''
+
         price_by_night = getattr(self.new_place, "price_by_night")
         self.assertIsInstance(price_by_night, int)
 
     def test_type_max_guest(self):
-        '''
-            Test the type of max_guest
-        '''
+        '''Test the type of max_guest'''
+
         max_guest = getattr(self.new_place, "max_guest")
         self.assertIsInstance(max_guest, int)
 
     def test_type_number_bathrooms(self):
-        '''
-            Test the type of number_bathrooms
-        '''
+        '''Test the type of number_bathrooms'''
+
         number_bathrooms = getattr(self.new_place, "number_bathrooms")
         self.assertIsInstance(number_bathrooms, int)
 
     def test_type_number_rooms(self):
-        '''
-            Test the type of number_bathrooms
-        '''
+        '''Test the type of number_bathrooms'''
+
         number_rooms = getattr(self.new_place, "number_rooms")
         self.assertIsInstance(number_rooms, int)
 
     def test_type_description(self):
-        '''
-            Test the type of description
-        '''
+        '''Test the type of description'''
+
         description = getattr(self.new_place, "description")
         self.assertIsInstance(description, str)
 
     def test_type_name(self):
-        '''
-            Test the type of name
-        '''
+        ''' Test the type of name'''
+        
         name = getattr(self.new_place, "name")
         self.assertIsInstance(name, str)
